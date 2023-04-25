354920e97ca1bf5d0d2e0dcec06294a7a5697875
Michelle Garcia <michelletgar@gmail.com>
2023-04-25 13:26:07 -0600

    ci: metodos filter includes

--- js02/js02.js
+++ js02/js02.js
@@ -19,27 +19,25 @@ console.log("Sesion JS02");
     console.log("resultado");
 } */
 
-multiplica(5,10);
+multiplica(5, 10);
 
-function multiplica(operandoA,operandoB) {
-    const resultado = operandoA * operandoB;
-    console.log(resultado);
+function multiplica(operandoA, operandoB) {
+  const resultado = operandoA * operandoB;
+  console.log(resultado);
 }
 
 function divide(dividendo, divisor) {
-    return dividendo/divisor;
+  return dividendo / divisor;
 }
 
-console.log(divide(5,2));
-
+console.log(divide(5, 2));
 
 // Funciones expresadas
 // Function expressions
-// Son declaradas dentro de la asignacion de una variable 
+// Son declaradas dentro de la asignacion de una variable
 // Estas funciones pueden ser anónimas (no tienen nombre)
 // Las funciones expresadas no tiene hoisting, por que no se
 // carga en memoria hsta que se utilice
-
 
 /*
     const nombreVariable = function nombreFuncion(parametros) {
@@ -47,28 +45,24 @@ console.log(divide(5,2));
     }
 */
 
-const sum = function sumatoria(a,b) {
-    return a + b;
-}
+const sum = function sumatoria(a, b) {
+  return a + b;
+};
 
-const resta = function (a,b) {
-    return a - b;
-}
+const resta = function (a, b) {
+  return a - b;
+};
 
-
-
-console.log(sum(5,5));
-console.log(resta(10,5));
-
+console.log(sum(5, 5));
+console.log(resta(10, 5));
 
 // Funciones autoinvocadas
 // self-invoking functions
 // se puede definir con funciones anonimas
 
-(function (){
-    console.log("Me autoinvoco en la funcion");
-}) ();
-
+(function () {
+  console.log("Me autoinvoco en la funcion");
+})();
 
 // Funciones flechas
 // arrowFunctions
@@ -85,11 +79,119 @@ const areaRectangulo = function(base,altura) {
 
 /**
  * documentacion de la funcion
- * @param {number} base esto es la descripcion de base 
+ * @param {number} base esto es la descripcion de base
  * @param {number} altura esto es la descripcion de altura
  * @returns área del rectángulo
  */
 
 const areaRectangulo = (base, altura) => base * altura;
 
-console.log("Area de rectangulo de 5x3: ",areaRectangulo(5,3));
+console.log("Area de rectangulo de 5x3: ", areaRectangulo(5, 3));
+
+// Parametros rest
+
+// Rest Parameters    Sintaxis:  ...nombreParametro
+// NOs permite representar una serie de valores indefinidos en los argumentos
+// Estos se preseentan como un array
+
+function sumatoriaVariosNumeros(a, b, ...restoDatos) {
+  let suma;
+  suma = a + b;
+  for (let i = 0; i < restoDatos.length; i++) {
+    suma += restoDatos[i]; // suma = suma + rstoDatos[i];
+  }
+  return suma;
+}
+
+console.log("Sumatoria de varios numeros: " + sumatoriaVariosNumeros(5, 2));
+console.log(
+  "Sumatoria de varios numeros: " + sumatoriaVariosNumeros(5, 6, 9, 10)
+);
+console.log(
+  "Sumatoria de varios numeros: " + sumatoriaVariosNumeros(5, 6, 9, 10, 20, 50)
+);
+
+//  Funciones de Callback
+// Función que se pasa a orta función como argumento, para luego invocarse
+// para completar algún tipo de rutina o acción
+
+// 3 funciones, una que imprima en consola, DOM h1.
+// 1 parámetro de entrada
+// no tiene return.
+
+// Función que imprime un mensaje en la consola
+
+function printToConsole(mensaje) {
+  console.log(mensaje);
+}
+
+// Función que muestra una alerta en la pantalla
+function showAlert(mensaje) {
+  alert(mensaje);
+}
+
+// Función que agrega un encabezado h1 al DOM
+
+function addH1ToDOM(mensaje) {
+  const h1 = document.createElement("h1");
+  h1.innerText = mensaje;
+  document.body.appendChild(h1);
+}
+
+const addH1 = (mensaje) => {
+  document.querySelector("#div").innerHTML = `<h1>${mensaje}</h1>`;
+};
+
+function getName() {
+  const name = prompt("Dime tu nombre");
+  const text = `Hola ${name}, espero tengas un gran día`;
+  printToConsole(text);
+}
+
+getName();
+
+function double(arr) {
+  const doubleNumbers = [];
+  for (let i = 0; i < arr.length; i++) {
+    const duplicate = arr[i] * 2;
+    doubleNumbers.push(duplicate);
+  }
+  return doubleNumbers;
+}
+
+/*
+  Ejercicio 4
+  Crear un programa que itere sobre dos arreglos;
+  si hay cursos en común, imprimirlos en la consola.
+*/
+
+const student1Courses = [
+  "Math",
+  "English",
+  "Programming",
+  "Biology",
+  "Physics",
+  "Music",
+];
+const student2Courses = ["Geography", "Spanish", "Programming", "Music"];
+
+function cursosEnComun(student1Courses, student2Courses) {
+  const courses = [];
+  for (let i = 0; i < student1Courses.length; i++) {
+    for (let j = 0; j < student2Courses.length; j++) {
+      if (student1Courses[i] === student2Courses[j]) {
+        courses.push(student1Courses[i]);
+      }
+    }
+  }
+  return `Cursos en comun: ${courses}`;
+}
+
+function cursosEnComunV2(student1Courses, student2Courses) {
+    const courses = student1Courses.filter(course => student2Courses.includes(course));
+    return courses;
+}
+  
+
+// console.log(cursosEnComun(student1Courses, student2Courses));
+console.log(cursosEnComunV2(student1Courses, student2Courses));
