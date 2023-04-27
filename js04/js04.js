2f9cdef62c74a7517ada11de2100cbf826754bef
Michelle Garcia <michelletgar@gmail.com>
2023-04-27 10:09:49 -0600

    ci: matrices

---
+++ js04/js04.js
@@ -0,0 +1,9 @@
+
+
+// Matrices (arreglos de 2 dimensiones)
+
+const Arrayohortes = [
+/*ch26*/     ["Arath", "Jose", "Karen", "Sofi"],
+/*ch27*/     ["Manuel", "María", "Francisco", "Pablo"],
+/*ch28*/     ["Vennanzio", "Doris", "Brendan", "Lucas"]
+];
