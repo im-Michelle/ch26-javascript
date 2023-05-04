c477020fc88ef305879819b59b9757b5f60217c9
Michelle Trejo <michelletgar@gmail.com>
2023-05-04 17:31:48 -0600

    ci: api fetch

--- js07/apiFetch/js-07-apiFetch.js
+++ js07/apiFetch/js-07-apiFetch.js
@@ -1,1 +1,63 @@
 console.log("Js07 Api Fetch");
+/*
+La Api Fetch nos ayuda a acceder y manipular peticiones HTTP
+una forma logica y facil de obtener recursos de la red de manera asincrona
+
+Sintaxis: 
+Solicitud get
+
+fetch(  url  )
+    .then()
+    .catch();
+
+*/
+
+const url = "https://fakestoreapi.com/products";
+
+const getProducts = (url) => {
+    fetch(url)
+        .then(result => {
+            console.log(result.status)
+            return result.json();
+        })
+        .then((productsObj) => {
+            console.log("\nLista de productos:")
+            // Lista de productos
+            console.log(productsObj);
+            // Imprimir el titulo de cada producto
+            console.log("\nNombre productos:")
+            productsObj.forEach(product => {
+                console.log(product.title);
+            });
+        })
+        .catch(error => console.log(error));
+};
+
+getProducts(url);
+
+
+// Consumir el fetch usando async await 
+
+
+const getProductsAwait = async (url) => {
+    try {
+      const productsJson = await fetch(url);
+      console.log(productsJson.status);
+      const productsObj = await productsJson.json();
+      console.log("\nLista de productos:");
+      console.log(productsObj);
+      console.log("\nNombre productos:");
+  
+      localStorage.setItem("mis-productos", JSON.stringify(productsObj));
+  
+      productsObj.forEach((product) => {
+        console.log(product.title);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+
+
+getProductsAwait(url);
