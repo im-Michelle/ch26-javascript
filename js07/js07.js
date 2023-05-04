190779bdf90a54d06495dacb1e36c65a0245a4b8
Michelle Garcia <michelletgar@gmail.com>
2023-05-04 13:28:13 -0600

    ci: promises

--- js07/js07.js
+++ js07/js07.js
@@ -1,1 +1,80 @@
-console.log("Inicio de mi programa");
+// Desarrollo sincrono
+
+const primerPaso = () => {
+    console.log("01- Inicio de mi programa");
+};
+
+const segundoPaso = () => {
+    console.log("02- Desarrollo de mi programa");
+};
+
+const tercerPaso = () => {
+    console.log("03- Fin de mi programa");
+};
+
+primerPaso();
+segundoPaso();
+tercerPaso();
+
+
+// Programacion Asincrona
+// setTimeout()
+
+// Establee un temporizador que ejcuta
+// Una vez que expire el temporizador
+
+const desarrolloAsincrono = (saludo) => {
+    console.log("Hola " + saludo);
+};
+
+const segundoPasoAsincrono = (saludo) => {
+    /*setTimeout((saludoRef) => {
+        console.log("Hola "+ saludoRef);
+    }, 4000, saludo);*/
+    setTimeout(() => {
+        console.log("Hola "+ saludo);
+    }, 4000);
+};
+
+console.log("");
+primerPaso();
+// setTimeout(desarrolloAsincrono, 3000, "ch26");
+segundoPasoAsincrono("ch26 con 4 s. de retardo");
+tercerPaso();
+
+
+// setInterval
+// Ejecuta una funcion de manera reiterada con un tiempo de retardo fijo entre cada llamada
+
+const segundoPasoConIntervalo = (saludo) => {
+    setInterval(() => console.log("La banda malandra es " + saludo), 2000);
+};
+
+console.log("setInterval")
+primerPaso();
+segundoPasoConIntervalo("ch26 wu, wu");
+tercerPaso();
+
+// Iniciar y detener setInterval
+
+const refStartInterval = document.getElementById("iniciar");
+const refStopInterval = document.getElementById("detener");
+const refDateH2 = document.getElementById("fecha");
+let idInterval;
+refStartInterval.disable = true;
+
+refStartInterval.addEventListener("click", ()=> {
+    // console.log("Se pulsó iniciar");
+    setInterval(() => {
+        refDateH2.innerHTML = new Date().toLocaleString();
+    }, 1000);
+    refStartInterval.disable = true;
+    refStopInterval.disable = false;
+});
+
+refStopInterval.addEventListener("click", () => {
+    // console.log("Se pulsó detener");
+    clearInterval(idInterval); // detener mi intervalo
+    refStartInterval.disable = false;
+    refStopInterval.disable = true;
+});
---
+++ js07/promesas/js-07-promesas.js
@@ -0,0 +1,67 @@
+console.log("Js07 - promesas");
+
+/*  
+Una promesa (promise) es un objeto que se represnta la terminación o fracaso
+de una operacioón asincrona.
+
+Para crear una promesa se usa el constructor new Promise();
+*/
+
+const pinkiPromise = new Promise((fncCallBackRelsove, fncCallBackReject) => {
+    const menorEdad = false;
+
+    if (menorEdad)
+        fncCallBackRelsove("Hay que estar siempre juntos");
+    else
+        fncCallBackReject("No te conozco");
+});
+
+
+// Consumir las promesas
+// function then, catch y finally.
+
+console.log("Antes de consumir la promesa");
+
+pinkiPromise
+    .then((response) => console.log("Promesa resulta " + response))
+    .catch((exception) => console.error("Tu promesa fue rechazada " + exception));
+
+
+console.log("Despues de consumir la promesa");
+
+
+// Una promesa que reciba dos numeros A y B, y los divida
+// en caso de que b sesa cero, me envie un rechazo diciendome por que no se puede 
+// realizar la division 
+
+/*
+const divisionDosNumeros = new Promise((resolve, reject) => {
+    const a = 10;
+    const b = 0;
+
+    if (b === 0) {
+        reject("No se puede dividir entre cero");
+    } else {
+        resolve(a / b);
+    }
+});
+
+
+divisionDosNumeros
+    .then(response => console.log(response))
+    .catch(exception => console.error(exception));
+*/
+
+const divisionAsincrona = (a, b) => {
+    const myPromise = new Promise((resolve, reject) => {
+      if (b === 0) 
+        reject("No se puede dividir entre cero");
+      else 
+        resolve(a / b); 
+    });
+    return myPromise;
+};
+  
+divisionAsincrona(10, 2)
+    .then((response) => console.log(response))
+    .catch((exception) => console.error(exception));
